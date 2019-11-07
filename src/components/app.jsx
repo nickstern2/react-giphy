605cdf2f90a3efebe60b62073d436dbb6af075ac
nick stern <nicholas.stern2@gmail.com>
2019-11-06 20:55:23 -0500

    done

---
+++ src/components/app.jsx
@@ -0,0 +1,58 @@
+// Global imports
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
+// Internal imports
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+const GIPHY_API_KEY = 'NGbTORybmoT6MdCRHDXOGWkbFN4GiYb0';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
+    };
+  }
+
+  search = (query) => {
+    giphy({ apiKey: GIPHY_API_KEY, https: true })
+      .search({
+        q: query,
+        rating: 'g',
+        limit: 10
+      }, (err, result) => {
+        this.setState({
+          gifs: result.data
+        });
+      });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
